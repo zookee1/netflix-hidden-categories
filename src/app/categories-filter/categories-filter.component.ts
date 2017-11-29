37b51e209ac2c2e12f8f3ceaec34446046962249
Nick <github@nkls.cc>
2017-11-29 14:47:30 +0100

    still borked maybe

--- src/app/categories-filter/categories-filter.component.ts
+++ src/app/categories-filter/categories-filter.component.ts
@@ -8,9 +8,16 @@ import { HttpClient } from '@angular/common/http';
 @Pipe({ name: 'filter' })
 export class FilterPipe implements PipeTransform {
   public transform(values: any[], filter: string): any[] {
-    if (!values || !values.length) {
+    if (!values) {
        return [];
     }
+
+    if(values.categories) {
+      return Object.keys(values.categories);
+    } else {
+      values = [];
+    }
+
     if (!filter) {
       return values;
     }
@@ -24,7 +31,7 @@ export class FilterPipe implements PipeTransform {
   styleUrls: ['./categories-filter.component.css']
 })
 export class CategoriesFilterComponent implements OnInit {
-  filterString = '';
+  filterStrings = '';
   categories_object = {};
 
   constructor(private http: HttpClient) {
@@ -32,27 +39,7 @@ export class CategoriesFilterComponent implements OnInit {
   }
 
   public getJSON(): Observable<any> {
-    return this.http.get('assets/categories.json')
-    .map((res: Response) => {
-      if (res) {
-          if (res.status === 201) {
-              return [{ status: res.status, json: res }];
-          } else if (res.status === 200) {
-              return [{ status: res.status, json: res }];
-          }
-      }
-    }).catch((error: any) => {
-        if (error.status === 500) {
-            return Observable.throw(new Error(error.status));
-        } else if (error.status === 400) {
-            return Observable.throw(new Error(error.status));
-        } else if (error.status === 409) {
-            return Observable.throw(new Error(error.status));
-        } else if (error.status === 406) {
-            return Observable.throw(new Error(error.status));
-        }
-    });
-
+    return this.http.get('assets/categories.json');
   }
 
   ngOnInit() {}
