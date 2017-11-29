0c829900ea4cc5beb04e8febd49759f41f4e03a7
zookee1 <github@zookee.one>
2017-11-29 19:54:31 +0100

    working, but not case-insensitive

--- src/app/categories-filter/categories-filter.component.ts
+++ src/app/categories-filter/categories-filter.component.ts
@@ -3,24 +3,19 @@ import { Observable } from 'rxjs/Observable';
 import 'rxjs/add/operator/map';
 import 'rxjs/add/operator/catch';
 import { HttpClient } from '@angular/common/http';
+import * as _ from 'lodash';
 
 
 @Pipe({ name: 'filter' })
 export class FilterPipe implements PipeTransform {
   public transform(values: any[], filter: string): any[] {
-    if (!values) {
+    if (!values || !values.length) {
        return [];
     }
-
-    if(values.categories) {
-      return Object.keys(values.categories);
-    } else {
-      values = [];
-    }
-
     if (!filter) {
       return values;
     }
+
     return values.filter(v => v.indexOf(filter) >= 0);
   }
 }
@@ -32,14 +27,14 @@ export class FilterPipe implements PipeTransform {
 })
 export class CategoriesFilterComponent implements OnInit {
   filterStrings = '';
-  categories_object = {};
+  categories = {};
 
   constructor(private http: HttpClient) {
-    this.getJSON().subscribe(data => this.categories_object = data, error => console.log(error));
+    this.getJSON().subscribe(data => this.categories = data, error => console.log(error));
   }
 
   public getJSON(): Observable<any> {
-    return this.http.get('assets/categories.json');
+    return this.http.get('assets/categories_array.1.json');
   }
 
   ngOnInit() {}
